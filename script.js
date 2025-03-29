e16e34575b65ef6a8bd82c02f1f0c3d36edde743
joshua-mae <86929441+joshua-mae@users.noreply.github.com>
2025-03-29 14:26:39 -0400

    sidebar fix

--- script.js
+++ script.js
@@ -7,5 +7,3 @@ function closeSidebar() {
   const sidebar = document.querySelector(".sidebar");
   sidebar.style.display = "none";
 }
-window.showSidebar = showSidebar;
-window.closeSidebar = closeSidebar;
